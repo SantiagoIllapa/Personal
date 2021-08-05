b9ed42a7257255bf3cf892a9b451c0ea0b874ec8
Santiago <60227801+SantiagoIllapa@users.noreply.github.com>
2021-08-05 11:08:08 -0500

    Https protocol

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(express.static(__dirname + '/public'));
 //Express hbs view enginegg
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+app.use(function(req, res, next) {
+if ((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+res.redirect(307, 'https://' + req.get('Host') + req.url);
+} else
+next();
+});
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'cArLoS bRaVO'
